7fbca3805a0035a77dc748516aa6536989d7be54
Salehabk <saleha86@hotmail.co.uk>
2021-11-25 02:26:13 +0000

    New changes made

---
+++ src/test/java/com/qa/stocklist/test/controller/TestItem.java
@@ -0,0 +1,32 @@
+package com.qa.stocklist.test.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.BeforeClass;
+
+public class TestItem {
+
+	@Before
+	@After
+	@BeforeClass
+	@AfterClass
+	
+	
+	
+	@Test
+	
+	
+	@Test
+	
+	
+	
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
